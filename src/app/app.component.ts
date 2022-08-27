736c428cce53b74d84bb33fbf1793d163616f495
Kelley Bellard <kelley.bellard@gmail.com>
2022-08-27 16:35:25 -0600

    started entry/table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
 
+/**
+ * This holds info for a single subject
+ */
+ class Subject {
+  name: string;
+  start: string;
+  end: string;
+
+  constructor(name: string,start: string,  end: string) {
+    this.name = name;
+    this.start = start;
+    this.end = end;
+  }
+}
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'amethyst';
+
+  subjects: Subject[] = [];
+
+  targetEnd?: string;
+
+  // thisaldsfkj
+
+  /** subject entered by Lachlan on the UI */
+  subject?: string;
+  start?: string;
+  end?: string;
+
+  AddSubject() {
+    const subject =  new Subject(this.subject as string, this.start as string, this.end as string);
+    this.subjects.push(subject);
+  }
+
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,9 +11,12 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
