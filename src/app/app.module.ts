4b11d61e671d8354fe6237dde696e2156415f8e9
Kelley Bellard <kelley.bellard@gmail.com>
2022-08-27 14:52:29 -0600

    complete redo :)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { MatSliderModule } from '@angular/material/slider';
-
-import { MatToolbarModule } from '@angular/material/toolbar'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    MatSliderModule,
-    MatToolbarModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
